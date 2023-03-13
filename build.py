9f0c97a2d0a1a2eaec9bb7ecda2a1b580c83d81c
Kyle Treubig <kyle.treubig@twosixlabs.com>
2023-03-13 11:27:20 -0400

    Create python scripts to run build in container, and utilities for building

---
+++ build.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2023 Two Six Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+RACE external project builder script
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+import time
+from datetime import timedelta
+
+
+default_cache_dir = os.environ.get(
+    "EXT_CACHE_DIR",
+    f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/ext-cache"
+)
+build_lib_file = f"{os.path.dirname(os.path.abspath(__file__))}/race_ext_builder.py"
+
+
+def get_host_target() -> str:
+    """Get target corresponding to the current host"""
+    return "linux-x86_64" if "x86" in os.uname().machine else "linux-arm64-v8a"
+
+
+def get_cli_arguments():
+    """Parse command-line arguments to the script"""
+    parser = argparse.ArgumentParser(
+        description="Build external project",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "project",
+        help="Path to external project to build",
+    )
+    parser.add_argument(
+        "project_args",
+        help="Pass-through args for the project build script",
+        nargs="*",
+    )
+    parser.add_argument(
+        "--target",
+        choices=["linux-x86_64", "linux-arm64-v8a", "android-x86_64", "android-arm64-v8a"],
+        default=get_host_target(),
+        help="Target to build",
+    )
+    parser.add_argument(
+        "--image",
+        default="ext-builder", # TODO use ghcr.io image
+        help=f"Docker image to use for building the external project",
+        type=str,
+    )
+    parser.add_argument(
+        "--cache-dir",
+        default=default_cache_dir,
+        help="Location of cache dir",
+        type=str,
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print commands but do not execute",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output",
+    )
+    return parser.parse_intermixed_args()
+
+
+def run_build(
+    project_dir: str,
+    args: argparse.Namespace,
+):
+    cmd = [
+        "docker",
+        "run",
+        "--rm",
+        "-v",
+        f"{project_dir}:/build/code",
+        "-v",
+        f"{args.cache_dir}:/build/cache",
+        "-v",
+        f"{build_lib_file}:/usr/lib/python3.8/race_ext_builder.py",
+    ]
+    cmd.extend([
+        args.image,
+        "python3",
+        "-u",
+        "/build/code/build.py",
+        "--target",
+        args.target,
+    ])
+    if args.project_args:
+        cmd.extend(args.project_args)
+
+    if args.dry_run or args.verbose:
+        print(f"Executing: {' '.join(cmd)}")
+        print()
+    if not args.dry_run:
+        subprocess.run(cmd, check=True)
+
+
+if __name__ == "__main__":
+    args = get_cli_arguments()
+
+    project_dir = os.path.abspath(args.project)
+    if not os.path.exists(f"{project_dir}/build.py"):
+        print(f"build.py does not exist in {project_dir}")
+        sys.exit(1)
+
+    start_time = time.time()
+    try:
+        run_build(project_dir, args)
+    finally:
+        stop_time = time.time()
+        duration = timedelta(seconds=stop_time - start_time)
+        print(f"Took {duration} to execute")
---
+++ race_ext_builder.py
@@ -0,0 +1,225 @@
+#
+# Copyright 2023 Two Six Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse
+import logging
+import os
+import subprocess
+import sys
+from typing import Any, List, Mapping, Optional
+
+
+logger = logging.root
+
+
+def get_arg_parser(name: str, version: str, revision: int, caller: str) -> argparse.ArgumentParser:
+    """Get command-line arguments parser pre-configured for common arguments"""
+    parser = argparse.ArgumentParser(
+        description=f"Build {name}",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    # Args that shouldn't really need to ever be set by the user, but we
+    # allow for them to be overridden, and mainly use them as a means of
+    # passing them around
+    parser.add_argument(
+        "--name",
+        default=name,
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--code-dir",
+        default=os.path.dirname(os.path.abspath(caller)),
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--source-dir",
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--build-dir",
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--install-dir",
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--log-file",
+        help=argparse.SUPPRESS,
+    )
+    parser.add_argument(
+        "--pkg-file",
+        help=argparse.SUPPRESS,
+    )
+    # Real user-level args
+    parser.add_argument(
+        "--version",
+        default=version,
+        help=f"Version of {name}",
+        type=str,
+    )
+    parser.add_argument(
+        "--revision",
+        default=revision,
+        help=f"Build revision",
+        type=int,
+    )
+    parser.add_argument(
+        "--target",
+        choices=["linux-x86_64", "linux-arm64-v8a", "android-x86_64", "android-arm64-v8a"],
+        help="Target to build",
+        required=True,
+    )
+    parser.add_argument(
+        "--num-threads",
+        default=os.cpu_count(),
+        help="Number of concurrent jobs to run (set to # CPUs for optimal use)",
+        type=int,
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print commands but do not execute",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output",
+    )
+    return parser
+
+
+def normalize_args(args: argparse.Namespace) -> argparse.Namespace:
+    """Ensure all required args are set, using user-provided values to derive defaults"""
+    cache_dir = f"/build/cache/{args.name}/{args.version}-{args.revision}/{args.target}"
+    if not args.source_dir:
+        args.source_dir = f"{cache_dir}/source"
+    if not args.build_dir:
+        args.build_dir = f"{cache_dir}/build"
+    if not args.install_dir:
+        args.install_dir = f"{cache_dir}/install"
+    if not args.log_file:
+        args.log_file = f"/build/cache/{args.name}-{args.version}-{args.revision}-{args.target}.log"
+    if not args.pkg_file:
+        args.pkg_file = f"/build/cache/{args.name}-{args.version}-{args.revision}-{args.target}.tar.gz"
+    return args
+
+
+def make_dirs(args: argparse.Namespace):
+    """Create all cache directories"""
+    os.makedirs(args.source_dir, exist_ok=True)
+    os.makedirs(args.build_dir, exist_ok=True)
+    os.makedirs(args.install_dir, exist_ok=True)
+
+
+class ConsoleFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    green = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    format = "%(asctime)s (%(levelname)s): %(message)s"
+
+    default_formatter = logging.Formatter(grey + format + reset)
+    formatters = {
+        logging.DEBUG: logging.Formatter(grey + format + reset),
+        logging.INFO: logging.Formatter(green + format + reset),
+        logging.WARNING: logging.Formatter(yellow + format + reset),
+        logging.ERROR: logging.Formatter(red + format + reset),
+        logging.CRITICAL: logging.Formatter(bold_red + format + reset),
+    }
+
+    def format(self, record):
+        formatter = self.formatters.get(record.levelno, self.default_formatter)
+        return formatter.format(record)
+
+
+def setup_logger(args: argparse.Namespace):
+    """Set up python logger"""
+    logging.basicConfig(
+        filemode='w',
+        filename=args.log_file,
+        force=True,
+        format="%(asctime)s %(levelname)-8s : %(message)s",
+        level=logging.DEBUG,
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(ConsoleFormatter())
+    console_handler.setLevel(logging.DEBUG if args.dry_run or args.verbose else logging.INFO)
+    logging.root.addHandler(console_handler)
+
+
+def fetch_source(args: argparse.Namespace, source: Optional[str], extract: Optional[str]):
+    """Fetch (and optionally) """
+    base_filename = os.path.basename(source)
+    local_filepath = os.path.join(args.source_dir, base_filename)
+    if not os.path.exists(local_filepath):
+        logger.info(f"Fetching {source} to {local_filepath}")
+        execute(args, [
+            "wget",
+            f"--output-document={local_filepath}",
+            "--no-verbose",
+            source,
+        ])
+    else:
+        logger.debug(f"Using cached {local_filepath}")
+
+    if extract == "tar.gz":
+        logger.info(f"Extracting {source}")
+        execute(args, [
+            "tar",
+            "--extract",
+            f"--file={local_filepath}",
+            f"--directory={args.source_dir}",
+        ])
+
+
+def execute(args: argparse.Namespace, cmd: List[str], cwd: Optional[str] = None, env: Optional[Mapping[str, Any]] = None):
+    """Execute command"""
+    # Normalize command array
+    cmd = [str(c) for c in cmd]
+    # If env vars were provided, add them to execution environment
+    cmd_env = None
+    if env:
+        cmd_env = {k: v for (k,v) in os.environ}
+        cmd_env.update(env)
+
+    logger.debug(f"Executing: {' '.join(cmd)}")
+    if not args.dry_run:
+        proc = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=cmd_env, shell=False, universal_newlines=True)
+        for line in proc.stdout:
+            logger.debug(line.rstrip())
+        proc.wait()
+        if proc.returncode != 0:
+            raise Exception(f"Command {cmd} returned with non-zero exit status {proc.returncode}")
+
+
+def create_package(args: argparse.Namespace):
+    """Create tarball package"""
+    logger.info(f"Packaging {args.install_dir} into {args.pkg_file}")
+    execute(args, [
+        "tar",
+        "--create",
+        f"--file={args.pkg_file}",
+        "--gzip",
+        f"--directory={args.install_dir}",
+        ".",
+    ])
